efc06fc2a24b6a44f6ca1643e37a2dac8cb1e484
jonhels <jonhels8888@gmail.com>
2022-10-07 12:34:21 +0200

    learning to sort

--- script.sh
+++ script.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-curl -s https://folk.ntnu.no/gioeleba/idg1100/2022/ob1/people.txt | head -n 9 | wc -l
+wget  https://folk.ntnu.no/gioeleba/idg1100/2022/ob1/people.txt
+
+sleep 1
+
+tail -n29 $people
+
+
+#head -n8 temp.ch | sed -e "s/[0-9@//; s/[A-Z]/\L&/g;" | sort | uniq -u > .sorted.ch  
 
